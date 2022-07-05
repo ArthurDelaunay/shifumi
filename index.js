adb86a3f22a61b7970c17682ad7925b3e3db827f
ArthurDelaunay <delaunayfredericarthur@gmail.com>
2022-07-05 10:43:23 +0200

    scoreboard add

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
+// gestion du score
+const addToScore = () => {
+    scoreboard.innerHTML = `${playerScore} - ${opponentScore}`
+}
+const playerWin = () => {
+    playerScore += 1
+    addToScore()
+}
+const opponentWin = () => {
+    opponentScore += 1
+    addToScore()
+}
+
 // gestion de qui gagne la manche
 const whoWin = (playerResult) => {
     const opponentResult = automaticShifumi()
-    const shifumiValues = ["scissors", "paper", "stone", "scissors"] // [0, 1, 2]
+    const shifumiValues = ["scissors", "paper", "stone", "scissors"]
     let finalResult = ""
     if (playerResult === opponentResult) {
         finalResult += "draw !"
@@ -10,15 +23,16 @@ const whoWin = (playerResult) => {
         const playerResultIndex = shifumiValues.indexOf(playerResult)
         const opponentResultIndex = shifumiValues.indexOf(opponentResult)
         if (shifumiValues[playerResultIndex + 1] === shifumiValues[opponentResultIndex]) {
-            finalResult += `vous avez gagné avec ${playerResult}`
+            // finalResult += `vous avez gagné avec ${playerResult}`
+            playerWin()
         }
         else {
-            finalResult += `l'adversaire a gagné avec ${opponentResult}`
+            // finalResult += `l'adversaire a gagné avec ${opponentResult}`
+            opponentWin()
         }
     }
     console.log(finalResult) 
 }
-
 
 // Shifumi côté adversaire géré aléatoirement 
 const automaticShifumi = () => {
@@ -43,9 +57,16 @@ const stonePlayerChoice = () => {
 const scissorsButton = document.getElementById("scissorsButton")
 const paperButton = document.getElementById("paperButton")
 const stoneButton = document.getElementById("stoneButton")
+let scoreboard = document.getElementById("scoreboard")
+let playerScore = 0
+let opponentScore = 0
 
 // Event Listeners
 // Shifumi côté joueur 
 scissorsButton.addEventListener("click", scissorsPlayerChoice)
 paperButton.addEventListener("click", paperPlayerChoice)
 stoneButton.addEventListener("click", stonePlayerChoice)
+
+
+// pour ajouter image setAttribute
+// pour défilement rapide boucle + interval court
